6b30b930f5eb94c9996f05f22b47cadf5c11bbc9
Mahugu <isaacbihengoto@gmail.com>
2019-03-05 09:30:47 +0300

    included the Gamer function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ var gamerB = "";
 var tossDice = function() {
   return Math.floor(6 * Math.random()) + 1;
 };
+
+function Gamer(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.gamerName;
+}
